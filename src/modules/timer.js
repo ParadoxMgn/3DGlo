91b71c1235cac2fc739590d61ffea959585e1a9c
Eugene Shumkov <radoxmgn@gmail.com>
2022-06-21 16:59:30 +0300

    Complete tasks of the lesson19

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,4 +1,5 @@
 const timer = (deadline) => {
+  const timerDays = document.getElementById('timer-days');
   const timerHours = document.getElementById('timer-hours');
   const timerMinutes = document.getElementById('timer-minutes');
   const timerSeconds = document.getElementById('timer-seconds');
@@ -15,20 +16,37 @@ const timer = (deadline) => {
     return { days, hours, minuts, seconds, timeRemaining };
   };
 
+  const getCountDays = () => {
+    const getStartTime = getTimeRemaining();
+
+    if (getStartTime.days.toString().slice(-1) === '1' && getStartTime.days.toString().slice(-2) !== '11') {
+      return `${getStartTime.days} день`;
+    } else {
+      if (getStartTime.days.toString().slice(-1) === '2' && getStartTime.days.toString().slice(-2) !== '12' || getStartTime.days.toString().slice(-1) === '3' && getStartTime.days.toString().slice(-2) !== '13' || getStartTime.days.toString().slice(-1) === '4' && getStartTime.days.toString().slice(-2) !== '14') {
+        return `${getStartTime.days} дня`;
+      } else {
+        return `${getStartTime.days} дней`;
+      }
+    }
+  };
+
   const showStartTime = () => {
     const getStartTime = getTimeRemaining();
+
+    timerDays.innerText = getStartTime.days === 0 ? `` : getCountDays();
     timerHours.innerText = getStartTime.hours < 10 ? `0${getStartTime.hours}` : getStartTime.hours;
     timerMinutes.innerText = getStartTime.minuts < 10 ? `0${getStartTime.minuts}` : getStartTime.minuts;
     timerSeconds.innerText = getStartTime.seconds < 10 ? `0${getStartTime.seconds}` : getStartTime.seconds;
   };
 
   const updateClock = () => {
-    const getTime = getTimeRemaining();
+    const getStartTime = getTimeRemaining();
 
-    if (getTime.timeRemaining > 0) {
-      timerHours.innerText = getTime.hours < 10 ? `0${getTime.hours}` : getTime.hours;
-      timerMinutes.innerText = getTime.minuts < 10 ? `0${getTime.minuts}` : getTime.minuts;
-      timerSeconds.innerText = getTime.seconds < 10 ? `0${getTime.seconds}` : getTime.seconds;
+    if (getStartTime.timeRemaining > 0) {
+      timerDays.innerText = getStartTime.days === 0 ? `` : getCountDays();
+      timerHours.innerText = getStartTime.hours < 10 ? `0${getStartTime.hours}` : getStartTime.hours;
+      timerMinutes.innerText = getStartTime.minuts < 10 ? `0${getStartTime.minuts}` : getStartTime.minuts;
+      timerSeconds.innerText = getStartTime.seconds < 10 ? `0${getStartTime.seconds}` : getStartTime.seconds;
     } else {
       timerHours.innerText = '00';
       timerMinutes.innerText = '00';
