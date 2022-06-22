6827aa65ed1c4eb4ba94c43544cd35b8e12ce160
Eugene Shumkov <radoxmgn@gmail.com>
2022-06-22 20:17:26 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import scrolling from './modules/scroll';
-import arrow from './modules/arrow'
+import arrow from './modules/arrow';
 
 timer('29 june 2022 20:00:00');
 menu();
